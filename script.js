e33776f529c1f7bebd53e922a81d17b230b0539e
SriHarsha3125 <harshagattu31@gmail.com>
2024-04-18 19:06:00 -0500

    adding of the dark mode

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ cloneContainer.id = 'dark-container';
 document.body.appendChild(cloneContainer);
 cloneContainer.classList.remove('active');
 
+const toggleIcons = document.querySelectorAll('.toggle-icon');
+const icons = document.querySelectorAll('.toggle-icon i');
+const darkContainer = document.querySelector('#dark-container');
+
 const darkContainerImg = document.querySelector('#dark-container .home-img img');
 
 darkContainerImg.src = 'digital-passport.jpg';
+
+toggleIcons.forEach(toggle =>{
+    toggle.addEventListener('click',() =>{
+
+        toggle.classList.add('disabled');
+
+        setTimeout(() => {
+            toggle.classList.add('disabled');
+        }, 1500);
+        icons.forEach(icon => {
+            icon.classList.toggle('bx-sun');
+        });
+
+        container.classList.toggle('active');
+        darkContainer.classList.toggle('active');
+    });
+});
